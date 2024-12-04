d742f18a30c777e18a291881e5c476725a62b907
Rupesh Thakuri <63655403+RupeshThakuri@users.noreply.github.com>
2024-12-04 14:00:16 +0545

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -56,7 +56,7 @@ function Contact() {
                 <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
                     <div className="max-w-2xl mx-auto text-center">
                         <h2 className="text-3xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl">Contact us</h2>
-                        <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-500">If you have any questions or concerns, please don't hesitate to contact us.</p>
+                        <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-500">"If you have any questions or concerns, please don't hesitate to contact us."</p>
                     </div>
 
                     <div className="max-w-5xl mx-auto mt-12 sm:mt-16">
